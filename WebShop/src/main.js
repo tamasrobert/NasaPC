0a8661d1a8d32c6dc19a606e9782c9d89003a54c
Erik <erik.raboczki98@gmail.com>
2022-01-31 10:38:38 +0100

    Changes in the folder structure

--- WebShop/src/main.js
+++ WebShop/src/main.js
@@ -1,11 +1,12 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router/router.js'
 
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import './assets/css/style.css'
 import './assets/css/Navbar.css'
 
-import router from './router.js'
+
 
 createApp(App).use(router).mount('#app')
