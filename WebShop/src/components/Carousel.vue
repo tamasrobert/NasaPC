b4aeb6fc81569c64068691758982ac22e7963b3f
Erik <erik.raboczki98@gmail.com>
2022-03-17 09:41:08 +0100

    Carousel works better now I only have to finish some remaining code.

--- WebShop/src/components/Carousel.vue
+++ WebShop/src/components/Carousel.vue
@@ -2,7 +2,7 @@
     <div id="carouselMainPage" class="carousel carousel-dark slide" data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselMainPage" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
-            <button type="button" v-for="(item,i) in carousel_items" :key="i+1" data-bs-target="#carouselMainPage" data-bs-slide-to="{{:key}}" aria-label="Slide 2"/>
+            <button type="button" v-for="(item,i) in carousel_items" :key="i" data-bs-target="#carouselMainPage" :data-bs-slide-to='i+1' :aria-label="'Slide' + i+2"/>
         </div>
         <!-- <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselMainPage" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
@@ -11,24 +11,10 @@
         </div> -->
         <div class="carousel-inner">
             <div class="carousel-item active" data-bs-interval="10000">
-                <img src="..." class="d-block w-100" alt="...">
+                <img src="https://picsum.photos/600/500" class="d-block w-100" alt="...">
                 <div class="carousel-caption d-none d-md-block">
                     <h5>First slide label</h5>
                     <p>Some representative placeholder content for the first slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item" data-bs-interval="2000">
-                <img src="..." class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Second slide label</h5>
-                    <p>Some representative placeholder content for the second slide.</p>
-                </div>
-            </div>
-            <div class="carousel-item">
-                <img src="..." class="d-block w-100" alt="...">
-                <div class="carousel-caption d-none d-md-block">
-                    <h5>Third slide label</h5>
-                    <p>Some representative placeholder content for the third slide.</p>
                 </div>
             </div>
         </div>
@@ -41,6 +27,7 @@
             <span class="visually-hidden">Next</span>
         </button>
     </div>
+
 </template>
 
 <script>
@@ -49,10 +36,10 @@ export default {
     data() {
         return{
             carousel_items: [
-                {data_slide:1, aria_slide:2},
-                {data_slide:2, aria_slide:3},
-                {data_slide:3, aria_slide:4},
-                {data_slide:4, aria_slide:5}
+                {title: "Carousel item 1", text: "Some placeholder text to fill the space. Enjoy!", imageURl: "https://picsum.photos/600/500"},
+                {title: "Carousel item 1", text: "Some placeholder text to fill the space. Enjoy!", imageURl: "https://picsum.photos/600/500"},
+                {title: "Carousel item 1", text: "Some placeholder text to fill the space. Enjoy!", imageURl: "https://picsum.photos/600/500"},
+                {title: "Carousel item 1", text: "Some placeholder text to fill the space. Enjoy!", imageURl: "https://picsum.photos/600/500"}
             ]
         }
     }
--- WebShop/src/views/Shop/Home.vue
+++ WebShop/src/views/Shop/Home.vue
@@ -1,6 +1,7 @@
 <template>
   <div class="container body">
     <Navbar/>
+    <Carousel/>
     <h1 style="color: white">This is the main page!</h1>
     <Footer/>
   </div>
@@ -9,11 +10,14 @@
 <script>
 import Navbar from '../../components/Navbar.vue'
 import Footer from '../../components/Footer.vue'
+import Carousel from '../../components/Carousel.vue'
+
 export default {
   name: 'Home',
   components: {
     Navbar,
-    Footer
+    Footer,
+    Carousel
   }
 
 }
