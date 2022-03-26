1e60ee4bd7db255acac889f195c249f4f811bc44
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-03-26 18:05:45 +0100

    bcrypt, nodemailer installed, auth.js - basics started

---
+++ WebShop_backend/backEndAPI/controllers/auth.js
@@ -0,0 +1,50 @@
+const User = require('../models/user');
+const nodemailer = require('nodemailer');
+const nodemailerSendgrid = require('nodemailer-sendgrid');
+const bcrypt = require('bcrypt');
+//const makeid = require('../util/makeid.js');
+require("dotenv").config();
+
+const transport = nodemailer.createTransport(
+    nodemailerSendgrid({
+        apiKey: process.env.SENDGRID_API_KEY
+    })
+  );
+
+exports.register = (req, res, next) => {
+    try {
+        if(req.body.email && req.body.password) {
+            let email = req.body.email;
+            db.collection('users').findOne({email})
+                .then(async (response) => {
+                    if(!response) {
+                        let password = await bcrypt.hash(req.body.password, 5);
+                        const activatorToken = makeid(32);
+                        db.collection('users').insertOne({email, password, activatorToken})
+                            .then((response2) => {
+
+                                transport.sendMail({
+
+                                });
+
+                                return res.send(response2);
+                            })
+                            .catch((error) => {
+                                return res.send(error);
+                            })
+                    } else {
+                        res.statusMessage = "Foglalt felhasználónév!";
+                        return res.sendStatus(400).end();
+                    }
+                })
+                .catch((error) => {
+                    console.log(error)
+                })
+        } else {
+            res.statusMessage = "Hiba! nincsenek feldolgozható adatok!";
+            return res.sendStatus(400).end();
+        }
+    } catch(e) {
+        res.statusCode(500);
+    }
+};
--- WebShop_backend/backEndAPI/routes/authentication.js
+++ WebShop_backend/backEndAPI/routes/authentication.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
+// import authentication controller
+const authController = require('../controllers/auth');
+
+// register a new user
+router.post('/api/register', authController.register);
 
 module.exports = router;
