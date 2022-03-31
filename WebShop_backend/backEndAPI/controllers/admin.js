e51f4f18d2b3ee042185b02751932b5eb56ec66f
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-03-31 16:50:09 +0200

    addProduct started, busboy is bugged, fix needed

--- WebShop_backend/backEndAPI/controllers/admin.js
+++ WebShop_backend/backEndAPI/controllers/admin.js
@@ -1,5 +1,8 @@
 const User = require('../models/user');
 const Product = require('../models/product');
+const makeid = require('../util/makeid.js');
+const busboy = require('busboy');
+const multer = require("multer");
 
 exports.addProductNoImage = (req, res) => {
     const session = req.cookies['LOCAL_KEY'];
@@ -18,4 +21,24 @@ exports.addProductNoImage = (req, res) => {
             .catch((error) => {return res.send(JSON.stringify(error))})
         })
         .catch((error) => {res.send(error)})
+}
+
+exports.addProduct = (req, res) => {
+    const session = req.cookies['LOCAL_KEY'];
+    if(!session) return res.sendStatus(401);
+    User.findOne({session, 'admin':true})
+        .then((response) => {
+            if(!response) return res.sendStatus(401);
+            var name = '';
+            var description = '';
+            var price = '';
+            var category = '';
+            // var generatedFileName = makeid(6) + ".jpg";
+            //if (!req.is('multipart/form-data')) {return res.sendStatus(415)};
+            
+            return res.sendStatus(201);
+        })
+        .catch((error) => {
+            res.send(error);
+        })
 }
--- WebShop_backend/backEndAPI/routes/admin.js
+++ WebShop_backend/backEndAPI/routes/admin.js
@@ -4,6 +4,9 @@ var router = express.Router();
 const adminController = require('../controllers/admin');
 
 // insert/add/create a new product --- no image selected -> placeholder - public/NoImage.png
-router.post('/api/products/add-product-no-image', adminController.addProductNoImage);
+router.post('/api/admin/add-product-no-image', adminController.addProductNoImage);
+
+// insert/add/create a new product --- has image selected
+router.post('/api/admin/add-product', adminController.addProduct);
 
 module.exports = router;
