2360ab09abe10448c21f68073a59ea94f2ecafb0
Erik <erik.raboczki98@gmail.com>
2022-04-07 12:08:40 +0200

    Some changes made in school, many changes were discarded because of errors

--- WebShop/src/views/Account/Signup.vue
+++ WebShop/src/views/Account/Signup.vue
@@ -1,184 +1,170 @@
 <template>
-    <main>
-        <Navbar/>
-        <div class="row formAndInfobox">
-            
-            <div class="col-xs-12 col-lg-6" style="margin-bottom: 2em">
-                <div class="form-demo">
-                    
-
-                    <div class="flex justify-content-center">
-                        <div class="card">
-                            <h5> Note: Frontend validation is missing</h5>
-                            <h5 class="text-center">Register</h5>
-                            <form class="p-fluid" method="post" @submit.prevent="SignupNewUser">
-                                <div class="field">
-                                    <div class="p-float-label p-input-icon-right">
-                                        <i class="pi pi-user"></i>
-                                        <InputText id="name" v-model="UserData.name" />
-                                        <!-- <label for="name" >Name*</label> -->
-                                    </div>
-                                    <!-- <small v-if="(v$.name.$invalid && submitted) || v$.name.$pending.$response" class="p-error">{{v$.name.required.$message.replace('Value', 'Name')}}</small> -->
-                                </div>
-                                <div class="field">
-                                    <div class="p-float-label p-input-icon-right">
-                                        <i class="pi pi-envelope" />
-                                        <InputText id="email" v-model="UserData.email"/>
-                                        <!-- <label for="email" >Email*</label> -->
-                                    </div>
-                                    
-                                
-                                </div>
-                                <div class="field">
-                                    <div class="p-float-label">
-                                        <Password id="password" v-model="UserData.password">
-                                            <template #header>
-                                                <h6>Pick a password</h6>
-                                            </template>
-                                            <template #footer="sp">
-                                                {{sp.level}}
-                                                <Divider />
-                                                <p class="mt-2">Suggestions</p>
-                                                <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                                                    <li>At least one lowercase</li>
-                                                    <li>At least one uppercase</li>
-                                                    <li>At least one numeric</li>
-                                                    <li>Minimum 8 characters</li>
-                                                </ul>
-                                            </template>
-                                        </Password>
-                                        <!-- <label for="password">Password*</label> -->
-                                    </div>
-                                </div>
-                                <div class="field">
-                                <span class="p-float-label">
-                                    <InputText id="inputtext" type="text" v-model="UserData.passwordAgain" />
-                                    <!-- <label for="inputtext">Password Again*</label> -->
-                                </span>
-                                </div>
-                                <div class="field">
-                                    <div class="p-float-label">
-                                        <Calendar id="date" v-model="UserData.date" :showIcon="true" />
-                                        <!-- <label for="date">Birthday</label> -->
-                                    </div>
-                                </div>
-                                <!-- <div class="field">
-                                    <div class="p-float-label">
-                                        <Dropdown id="country" v-model="country" :options="countries" optionLabel="name" />
-                                        <label for="country">Country</label>
-                                    </div>
-                                </div> -->
-                                <div class="field-checkbox">
-                                    <Checkbox/>
-                                    <label>I agree to the terms and conditions*</label>
-                                </div>
-                                <Button type="submit" label="Submit" class="mt-2" />
-                            </form>
+  <Navbar/>
+            <div class="form-demo">
+                <!-- <Dialog v-model:visible="showMessage" :breakpoints="{ '960px': '80vw' }" :style="{ width: '30vw' }" position="top">
+                    <div class="flex align-items-center flex-column pt-6 px-3">
+                        <i class="pi pi-check-circle" :style="{fontSize: '5rem', color: 'var(--green-500)' }"></i>
+                        <h5>Registration Successful!</h5>
+                        <p :style="{lineHeight: 1.5, textIndent: '1rem'}">
+                            Your account is registered under name <b>{{name}}</b> ; it'll be valid next 30 days without activation. Please check <b>{{email}}</b> for activation instructions.
+                        </p>
+                    </div>
+                    <template #footer>
+                        <div class="flex justify-content-center">
+                            <Button label="OK" @click="toggleDialog" class="p-button-text" />
                         </div>
+                    </template>
+                </Dialog> -->
+
+                <div class="flex justify-content-center">
+                    <div class="card">
+                        <h5 class="text-center">Register</h5>
+                        <form @submit.prevent="handleSubmit(!v$.$invalid)" class="p-fluid">
+                            <div class="field">
+                                <div class="p-float-label">
+                                    <InputText id="name" v-model="v$.name.$model" :class="{'p-invalid':v$.name.$invalid && submitted}" />
+                                    <label for="name" :class="{'p-error':v$.name.$invalid && submitted}">Name*</label>
+                                </div>
+                                <small v-if="(v$.name.$invalid && submitted) || v$.name.$pending.$response" class="p-error">{{v$.name.required.$message.replace('Value', 'Name')}}</small>
+                            </div>
+                            <div class="field">
+                                <div class="p-float-label p-input-icon-right">
+                                    <i class="pi pi-envelope" />
+                                    <InputText id="email" v-model="v$.email.$model" :class="{'p-invalid':v$.email.$invalid && submitted}" aria-describedby="email-error"/>
+                                    <label for="email" :class="{'p-error':v$.email.$invalid && submitted}">Email*</label>
+                                </div>
+                                <span v-if="v$.email.$error && submitted">
+                                    <span id="email-error" v-for="(error, index) of v$.email.$errors" :key="index">
+                                    <small class="p-error">{{error.$message}}</small>
+                                    </span>
+                                </span>
+                                <small v-else-if="(v$.email.$invalid && submitted) || v$.email.$pending.$response" class="p-error">{{v$.email.required.$message.replace('Value', 'Email')}}</small>
+                            </div>
+                            <div class="field">
+                                <div class="p-float-label">
+                                    <Password id="password" v-model="v$.password.$model" :class="{'p-invalid':v$.password.$invalid && submitted}" toggleMask>
+                                        <template #header>
+                                            <h6>Pick a password</h6>
+                                        </template>
+                                        <template #footer="sp">
+                                            {{sp.level}}
+                                            <Divider />
+                                            <p class="mt-2">Suggestions</p>
+                                            <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
+                                                <li>At least one lowercase</li>
+                                                <li>At least one uppercase</li>
+                                                <li>At least one numeric</li>
+                                                <li>Minimum 8 characters</li>
+                                            </ul>
+                                        </template>
+                                    </Password>
+                                    <label for="password" :class="{'p-error':v$.password.$invalid && submitted}">Password*</label>
+                                </div>
+                                <small v-if="(v$.password.$invalid && submitted) || v$.password.$pending.$response" class="p-error">{{v$.password.required.$message.replace('Value', 'Password')}}</small>
+                            </div>
+                            <div class="field">
+                                <div class="p-float-label">
+                                    <Calendar id="date" v-model="date" :showIcon="true" />
+                                    <label for="date">Birthday</label>
+                                </div>
+                            </div>
+                            <div class="field">
+                                <div class="p-float-label">
+                                    <Dropdown id="country" v-model="country" :options="countries" optionLabel="name" />
+                                    <label for="country">Country</label>
+                                </div>
+                            </div>
+                            <div class="field-checkbox">
+                                <Checkbox id="accept" name="accept" value="Accept" v-model="v$.accept.$model" :class="{'p-invalid':v$.accept.$invalid && submitted}" />
+                                <label for="accept" :class="{'p-error': v$.accept.$invalid && submitted}">I agree to the terms and conditions*</label>
+                            </div>
+                            <Button type="submit" label="Submit" class="mt-2" />
+                        </form>
                     </div>
                 </div>
             </div>
-
-
-
-
-            <div class="col-xs-12 col-lg-6 ">
-                <h4>Create and account and enjoy the following perks:</h4>
-                <ul>
-                    <li>
-                        <p>Get notifications about the new discounts, so you won't miss out on any awesome deal we have.</p>
-                    </li>
-                    <li>
-                        <p>Make a whislist and we will make sure inform you about your dream product.</p>
-                    </li>
-                    <li>
-                        <p>Collect loyality points and turn them in for extra bonuses.</p>
-                    </li>
-                    <li>
-                        <p>Make purchases faster by saving your billing information.</p>
-                    </li>
-                    <li>
-                        <p>Get the maximum experience out of your stay here.</p>
-                    </li>
-                </ul>
-            </div>
-
-
-
-
-        </div>
-        <Footer/>
-    </main>
+        
+  <Footer/>
 </template>
 
 <script>
 import Navbar from '../../components/Navbar.vue'
 import Footer from '../../components/Footer.vue'
-// import { email, required } from "@vuelidate/validators"
-// import { useVuelidate } from "@vuelidate/core"
-import Button from 'primevue/button'
-import Checkbox from 'primevue/checkbox'
-import Calendar from 'primevue/calendar'
-import Password from 'primevue/password'
-import InputText from 'primevue/inputtext'
-import AccountDataService from '../../services/AccountDataService.js'
-
+import { email, required } from "@vuelidate/validators";
+import { useVuelidate } from "@vuelidate/core";
 
 export default {
     name: 'Signup',
-    components: {
+     components: {
         Navbar,
         Footer,
-        Button,
-        Checkbox,
-        Calendar,
-        Password,
-        InputText,
     },
+    setup: () => ({ v$: useVuelidate() }),
     data() {
         return {
-            UserData: {
-                // name: 'Name',
-                email: 'E-mail',
-                password: 'Password',
-                // passwordAgain: 'PasswordAgain',
-                // date: ''
+            name: '',
+            email: '',
+            password: '',
+            date: null,
+            country: null,
+            accept: null,
+            submitted: false,
+            countries: null,
+            showMessage: false
+        }
+    },
+    validations() {
+        return {
+            name: {
+                required
+            },
+            email: {
+                required,
+                email
+            },
+            password: {
+                required
+            },
+            accept: {
+                required
             }
         }
     },
-    // validations() {
-    //     return {
-    //         name: {
-    //             required
-    //         },
-    //         email: {
-    //             required,
-    //             email
-    //         },
-    //         password: {
-    //             required
-    //         },
-    //         accept: {
-    //             required
-    //         }
-    //     }
-    // },
-    methods:{
-        SignupNewUser() {
-            AccountDataService.SignUp(this.UserData).then(() => {}).catch()
-            //ToDo: trigger a message which says to check emals !!!
-        }
+    methods: {
+        handleSubmit(isFormValid) {
+            this.submitted = true;
 
+            if (!isFormValid) {
+                return;
+            }
+
+            this.toggleDialog();
+        },
+        toggleDialog() {
+            this.showMessage = !this.showMessage;
+        
+            if(!this.showMessage) {
+                this.resetForm();
+            }
+        },
+        resetForm() {
+            this.name = '';
+            this.email = '';
+            this.password = '';
+            this.date = null;
+            this.country = null;
+            this.accept = null;
+            this.submitted = false;
+        }
     }
-    
-    
+
 }
 </script>
 
 <style lang="scss" scoped>
 .form-demo {
     .card {
-        min-width: 300px;
+        min-width: 450px;
 
         form {
             margin-top: 2rem;
@@ -194,9 +180,6 @@ export default {
             width: 80%;
         }
     }
-}
-.formAndInfobox {
-    margin: 5%;
 }
 
 </style>
