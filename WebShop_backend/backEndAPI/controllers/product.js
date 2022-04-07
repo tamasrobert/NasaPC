693cb4baa1b7156f48e1ec005def6ff8b477d7f1
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-04-07 20:34:31 +0200

    products.js controller readability improved

--- WebShop_backend/backEndAPI/controllers/product.js
+++ WebShop_backend/backEndAPI/controllers/product.js
@@ -1,16 +1,18 @@
 const Product = require('../models/product');
 
 exports.getAllProducts = (req, res) => {
-  Product
-  .find()
-  .then(products => {res.send(JSON.stringify(products))})
-  .catch((error) => {res.send(JSON.stringify(error))})
+
+  Product.find()
+    .then(products => res.json(products))
+    .catch((error) => res.json(error))
+
 };
 
 exports.getProductById = (req, res) => {
+
   let id = req.params.productId;
-  Product
-  .find(id)
-  .then(product => {res.send(JSON.stringify(product))})
-  .catch((error) => {res.send(JSON.stringify(error))})
+  Product.find(id)
+    .then(product => res.json(product))
+    .catch((error) => res.json(error))
+
 };
