a2b64f0d5ea6b0c7e997a4591736fa34ce8e013a
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-04-18 16:25:48 +0200

    order.js - improved returned messages

--- WebShopBackend/controllers/order.js
+++ WebShopBackend/controllers/order.js
@@ -47,7 +47,7 @@ exports.placeOrder = (req, res) => {
 
                 if (!user) {
                     Order.insertMany({ id, 'created_at': Date.now(), 'status': 'Processing...', ...req.body })
-                        .then(() => { return res.status(200).json({"message":"Order placed!"}) })
+                        .then(() => { return res.status(200).json({ "message": "Order placed!" }) })
                         .catch(err => res.send(err));
                 }
 
@@ -81,40 +81,39 @@ exports.placeOrder = (req, res) => {
                         transport.sendMail({
                             from: 'tamas.robert1@students.jedlik.eu',
                             to: 'tamasrobert00@gmail.com',
-                            subject: 'Order posted!',
+                            subject: 'Order placed!',
                             html: '<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta http-equiv="X-UA-Compatible" content="IE=edge"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>Document</title> <style> .container { background-color: #1d1d30; width: 800px; color: #fff; margin: 0 auto 0 auto; } h1,h3 { text-align: center; } h1 { padding-top: 30px; } .pt { padding-top: 30px; padding-left: 40px; text-align: start; } .pb { padding-bottom: 40px; } span { display: block; padding: 3px 5px 3px 40px; color: #fff; } table { width: 100%; padding: 0px 30px 30px 30px; margin: 0 auto 0 auto; } table tr { background-color: #151522; } table td { text-align: center; padding: 5px; } img { width: 48px; height: 48px; } .image { width: 48px; padding: 0; margin: 0; } .strip {background-color: #181827;} .total{background-color: #292945;} </style> </head> <body> <div class="container"> <h1>Your order has been recieved by us!</h1> <h3>Dear ' + req.body.last_name + ' ' + req.body.first_name + '!</h3> <span>Thank you for using our webshop!</span> <span>If you chosed pre-payment. You shall transfer the purchase price on the basis of the fee request sent by the following e-mail, writing the exact order ID in a notice: <b>' + id + '</b> Our bank account number: <b>12345678-12345678-12345678</b> Owner: <b>NasaPC</b> Important! If you have not received a fee request, please contact our customer service team before making a transfer.</span> <h3>Your order ID: ' + id + '</h3> <h3 class="pt">Personal information:</h3> <table> <tr> <td>Customer:</td> <td>' + req.body.last_name + ' ' + req.body.first_name + '</td> </tr> <tr class="strip"> <td>Email:</td> <td>' + req.body.email + '</td> </tr> <tr> <td>Contact phone number:</td> <td> ' + req.body.phone_number + ' </td> </tr> <tr class="strip"> <td>Billing address:</td> <td>' + req.body.billing_address + '</td> </tr> <tr> <td>Ship-to Address:</td> <td>' + req.body.shipping_address + '</td> </tr> </table> <h3 class="pt">Ordered products:</h3> <table class="products">' + htmlProducts + '</table> <span>Payment method: <b>' + payment(req.body.payment_method) + '</b></span> <span>Order date: <b>' + (Date(req.body.created_at)) + '</b></span> <span class="pb">Payable on receipt: <b>' + ((req.body.payment_method == 'cash_on_delivery') ? priceConverter(req.body.total_price) : '0') + ' Ft</b></span> </div> </body> </html>',
                             attachments: files
                         });
 
-                        return res.status(200).json({"message":"Order placed!"});
+                        return res.status(200).json({ "message": "Order placed!" });
                     })
-                    .catch(err => res.status(500).json({"error":"Unexpected error!"}));
+                    .catch(err => res.status(500).json({ "error": "Unexpected error!" }));
             })
-            .catch(err => res.status(404).json({"error":"User not found!"}))
+            .catch(err => res.status(404).json({ "error": "User not found!" }))
     }
 }
 
-exports.getUserOrders=(req,res)=>{
+exports.getUserOrders = (req, res) => {
     const session = req.cookies['LOCAL_KEY'];
     const userId = req.params.userId;
 
     if (session) {
         User.findOne({ session })
-        .then(user=>
-            {
+            .then(user => {
                 let id = user._id;
                 if (!user.courier && !user.admin) {
                     console.log("logged in as user")
-                    Order.find({id}).then(result=>{return res.status(200).json(result)}).catch(err=>res.status(404).json({"error":"Unexpected error!"}))
+                    Order.find({ id }).then(result => { return res.status(200).json(result) }).catch(err => res.status(404).json({ "error": "Unexpected error!" }))
                 }
                 else {
                     console.log("logged in as courier or admin")
-                    Order.find({userId}).then(result=>{return res.status(200).json(result)}).catch(err=>res.status(404).json({"error":"User does not exist!"}))
+                    Order.find({ userId }).then(result => { return res.status(200).json(result) }).catch(err => res.status(404).json({ "error": "User does not exist!" }))
                 }
             })
-        .catch(err=>console.log(err))
+            .catch(err => console.log(err))
     }
     else {
-        res.status(401).json({"error":"No session!"})
+        res.status(401).json({ "error": "No session!" })
     }
 }
