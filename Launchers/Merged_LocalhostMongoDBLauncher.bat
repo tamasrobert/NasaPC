917a74f24d07ed099676203432724c5bf1818f9c
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-04-21 17:02:21 +0200

    Updated README - formatting - added localhost db launcher

--- Launchers/Merged_LocalhostMongoDBLauncher.bat
+++ Launchers/Merged_LocalhostMongoDBLauncher.bat
@@ -8,4 +8,4 @@ cd C:\Program Files\MongoDB\Server\5.0\bin
 start cmd.exe /k "call mongod"
 echo Waiting for mongod to start...
 timeout /t 5 /nobreak
-start cmd.exe /C "call mongorestore --host 127.0.0.1 --db NasaPC "%userprofile%\Desktop\WebShopProject\Database\NasaPCDump" pause"
+start cmd.exe /C "call mongorestore --host 127.0.0.1 --db NasaPC "%userprofile%\Desktop\WebShopProject\Database\NasaPCDump""
