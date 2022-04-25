b366a3d1cb31867f5541c13d8da54d500ce28dc1
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-04-25 17:56:13 +0200

    product.js - deleteReview works

--- WebShopBackend/controllers/product.js
+++ WebShopBackend/controllers/product.js
@@ -41,7 +41,7 @@ exports.postReview = (req, res) => {
 
                                 var productReviews = [...product.reviews];
                                 var contains = productReviews.some(prod => {
-                                    return JSON.stringify(product._id) === JSON.stringify(prod._id);
+                                    return JSON.stringify(user._id) === JSON.stringify(prod.userId);
                                 });
 
                                 if (!contains) {
@@ -68,6 +68,66 @@ exports.postReview = (req, res) => {
             })
             .catch((error) => {
                 res.status(404).json({ "error": "User not found!" });
+            })
+    } else {
+        res.status(401).json({ "error": "No session!" });
+    }
+};
+
+exports.deleteReview = (req, res) => {
+
+    const session = req.cookies['LOCAL_KEY'];
+    const _id = req.params.productId;
+
+    if (session) {
+
+        User.findOne({ session })
+            .then((user) => {
+
+                if (user) {
+
+                    Product.findOne({ _id })
+                        .then(product => {
+
+
+                            let productExists = false;
+                            if (product) productExists = true
+
+                            let productInReviewsExists = false;
+
+                            for (let index = 0; index < product.reviews.length; index++) {
+
+                                if (String(user._id) === String(product.reviews[index].userId)) {
+                                    productInReviewsExists = true;
+                                    break;
+                                }
+                            }
+
+                            if (productExists && productInReviewsExists) {
+                                var filteredReviews = product.reviews.filter(function (value, index, arr) {
+                                    return (String(value.userId) != String(user._id));
+                                });
+
+                                Product.updateOne({ _id }, { $set: { reviews: filteredReviews } }).then(response => {
+                                    return res.status(200).json({ "message": "Reviews have been updated." })
+                                })
+                                    .catch(error => { return res.status(500).json({ "message": "Unexpected error!" }) })
+                            }
+                            else {
+
+                                if (productExists && !productInReviewsExists)
+                                    return res.status(404).json({ "error": "Review not found!" })
+                                else
+                                    return res.status(404).json({ "error": "Product not found!" })
+
+                            }
+
+                        })
+                        .catch(error => {console.log(error); res.status(404).json({ "error": "Unexpected error!" }) })
+                }
+            })
+            .catch((error) => {
+                return res.status(404).json({ "error": "Unexpected error!" });
             })
     } else {
         res.status(401).json({ "error": "No session!" });
