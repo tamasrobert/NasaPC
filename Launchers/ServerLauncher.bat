97013d6635052239b01fb05fb5df84552712f378
tamasrobert <tamas.robert1@students.jedlik.eu>
2022-04-27 21:52:44 +0200

    ServerLauncher - removed unwanted lines

--- Launchers/ServerLauncher.bat
+++ Launchers/ServerLauncher.bat
@@ -1,11 +1,8 @@
 @ECHO OFF 
 
-:: Section 1: Frontend
 cd ..\WebShop\WebShopFrontEnd\
 if not exist "node_modules\" call npm i
 start cmd.exe /k "call npm run serve"
-
-:: Section 1: Backend
 cd .\..\..\WebShopBackend\
 if not exist "node_modules\" call npm i
 start cmd.exe /k "call npm start"
